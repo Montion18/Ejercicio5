3997dc28d8364998b5b322554c598f24bf0f7987
Montion18 <62971959+Montion18@users.noreply.github.com>
2020-04-29 21:58:53 -0300

    Add files via upload

---
+++ Ejercicio5.py
@@ -0,0 +1,34 @@
+class alumno:
+    inasistmax=21
+    cantclases=56
+    __nombre=''
+    __anio=''
+    __division=''
+    __cantinasist=''
+
+    def __init__(self,nom='',anio='',divi='',inas=''):
+        self.__nombre=nom
+        self.__anio=anio
+        self.__division=divi
+        self.__cantinasist=inas
+
+    def retornaanio(self):
+        return self.__anio
+
+    def retornacant(self):
+        return self.__cantinasist
+
+    def retornainasis(self):
+        return self.inasistmax
+
+    def cambio(self,nueva):
+        self.inasistmax=nueva
+
+    def retornanom(self):
+        return self.__nombre
+
+    def retornacantclases(self):
+        return self.cantclases
+
+    def retornadivi(self):
+        return self.__division
